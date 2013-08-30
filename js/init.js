f038a208331f435006f7811cf4709064936705b5
Jay Baugh <jay.baugh@rackspace.com>
2013-08-30 17:18:51 -0400

    Chrome app working with AngularJS and templates.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('swift-share.html', {
     'bounds': {
-      'width': 1024,
-      'height': 768
+      'width': 640,
+      'height': 480
     }
   });
 });
